c39f39046b922a24c44a56e45e56c58cd66a262b
TrAnMu <Travis@wireless-10-105-6-217.umd.edu>
2017-02-15 12:01:34 -0500

    Picking up where we left off

---
+++ Tilt Maze/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.isKinematic == true) {
+			transform.Translate (Vector3.up * Time.deltaTime * 8.0f, Space.World);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			Application.Quit ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (0);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "Goal") {
+			rb.isKinematic = true;
+		}
+	}
+}
