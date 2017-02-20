91e685122f85001f55195ba4688a8a604495f4d3
TrAnMu <Travis@wireless-10-105-6-217.umd.edu>
2017-02-20 11:50:01 -0500

    Walls are turning...
    …sometimes more than I’d like…

--- Tilt Maze/Assets/Scripts/WallRotation.cs
+++ Tilt Maze/Assets/Scripts/WallRotation.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallRotation : MonoBehaviour {
+	private bool turning = false;
+	private bool clockwise;
+	private float startAngle;
+	private float endAngle;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +23,54 @@ public class WallRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!turning) {
+			if (Random.Range (0.0f, 1.0f) < Time.deltaTime / 10.0f) {
+				turning = true;
+				startAngle = transform.localRotation.eulerAngles.y;
+
+				if (Random.Range (0, 2) == 0) {
+					clockwise = true;
+					endAngle = startAngle + 90.0f;
+
+					if (endAngle >= 360.0) {
+						endAngle = 0.0f;
+					}
+				} else {
+					clockwise = false;
+					endAngle = startAngle - 90.0f;
+
+					if (endAngle < 0.0) {
+						endAngle = 270.0f;	
+					}
+				}
+			}
+		} else {
+			Turn (clockwise);
+		}
+	}
+
+	void Turn (bool clockwise) {
+		if (clockwise) {
+			transform.Rotate (Vector3.up, 90 * Time.deltaTime);
+
+			if (endAngle < startAngle && transform.rotation.eulerAngles.y < startAngle) {
+				transform.rotation.eulerAngles.Set (0.0f, 0.0f, 0.0f);
+				turning = false;
+			} else if (endAngle > startAngle && transform.rotation.eulerAngles.y > endAngle) {
+				transform.rotation.eulerAngles.Set (0.0f, endAngle, 0.0f);
+				turning = false;
+			}
+				
+		} else {
+			transform.Rotate (Vector3.up, -90 * Time.deltaTime);
+
+			if (endAngle > startAngle && transform.rotation.eulerAngles.y > startAngle) {
+				transform.rotation.eulerAngles.Set (0.0f, 0.0f, 0.0f);
+				turning = false;
+			} else if (endAngle < startAngle && transform.rotation.eulerAngles.y < endAngle) {
+				transform.rotation.eulerAngles.Set (0.0f, endAngle, 0.0f);
+				turning = false;
+			}
+		}
 	}
 }
